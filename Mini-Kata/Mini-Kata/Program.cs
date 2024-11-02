2b17d0775c52a17b31427f45dfb31af04bf187c0
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-02 20:05:40 +0100

    Minkata1

---
+++ Mini-Kata/Mini-Kata/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Arin";
+int hp = 100;
+float atkP = 15.5f;
+bool isParalysed = true;
+double gold = 24.75;
+
+Console.WriteLine($"Character Name: {name}");
+Console.WriteLine($"Health Points: {hp}");
+Console.WriteLine($"Attack Power: {atkP}");
+Console.WriteLine($"Is Paralysed: {isParalysed}");
+Console.WriteLine($"Gold Coins: {gold}");
