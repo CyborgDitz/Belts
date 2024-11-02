3fc518ca4207f2e492099ba9e8dde70aa14d2171
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-02 19:57:33 +0100

    Update Program.cs

--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -1,8 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("What is your name traveller?");
-string name = Console.Readline();
-Console.WriteLine($"Greetings {name}... My, you must have journeyed far.")
-    Console.WriteLine("But how dangerous was your journey on a scale of 1 - 10?")
-int dank = Int.Parse(Console.Readline());
+Console.WriteLine("Welcome to my potions store. What is your name traveller?");
+string name = Console.ReadLine();
+Console.WriteLine($"Greetings {name}... My, you must have journeyed far.");
+Console.WriteLine("But how dangerous was your journey on a scale of 1 - 10?");
 
+Dank();
+
+void Dank()
+{int dank = int.Parse(Console.ReadLine());
+    if (dank < 1 || dank > 10) {
+        Console.WriteLine("Oh come now, you can’t be serious? Try again, 1-10 how dangerous was it?");
+        Dank();
+    }
+    else {
+        switch (dank) {
+            case < 7:
+                Console.WriteLine("Oh then you will face much more exciting foes on ahead, traveller, ehehe");
+                break;
+            case >= 7:
+                Console.WriteLine("Oh my... Then you are mighty indeed, traveller.");
+                break;
+        }
+    }
+}
 
