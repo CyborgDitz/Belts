6728b4e70f24fe91cf30118bc626b1518180c43c
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-02 19:12:47 +0100

    Update Program.cs

--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name traveller?");
+string name = Console.Readline();
+Console.WriteLine($"Greetings {name}... My, you must have journeyed far.")
+    Console.WriteLine("But how dangerous was your journey on a scale of 1 - 10?")
+int dank = Int.Parse(Console.Readline());
+
+
