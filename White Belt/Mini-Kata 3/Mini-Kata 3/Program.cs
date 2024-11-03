7798eaa609a693327491828b6575fc4c86ebd0b5
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-03 14:31:00 +0100

    MiniKata 3 done

---
+++ White Belt/Mini-Kata 3/Mini-Kata 3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+void DoAttackBeast(){
+    Console.WriteLine("Do you want to attack the beast? (Y/N)");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "y"|| input.ToLower() == "yes")
+    {
+        Console.WriteLine("Attempting to attack the beast");
+        AttackRoll();
+    }
+    else
+    {
+        Console.WriteLine("You fail to muster courage in the hour of need... The beast continues its rampage.");
+    }
+}
+
+DoAttackBeast();
+
+void AttackRoll()
+{Random random = new Random();
+    int playerLuck = random.Next(1, 11);
+    if (playerLuck > 7)
+    {
+        Console.WriteLine("You hit the beast and slay it!");
+    }
+    else if (playerLuck == 6)
+    {
+        Console.WriteLine("You hit the beast, but the beast is too ravenous.");
+        DoAttackBeast();
+    }
+    else
+    {
+        Console.WriteLine("You fail to hit the beast.");
+        DoAttackBeast();
+    }
+}
