3b8fec9cda4abd103d01290f8ae7342de39b36f6
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-03 15:14:20 +0100

    Mini kata 4

---
+++ White Belt/Mini-Kata 4/Mini-Kata 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Spawning for loop enemies!");
+for (int i = 0; i < 7; i++)
+{
+    Console.WriteLine($"Enemmy #{i + 1} just spawned!");
+}
+Console.WriteLine("Spawning while loop enemies!");
+int enemies = 0;
+while (enemies < 5)
+{
+    Console.WriteLine($"Enemmy #{enemies + 1} just spawned!");
+ enemies++;   
+}
