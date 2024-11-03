bf7bb2d90968156fa3cdec78f0406fbf4926c154
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-03 15:57:04 +0100

    Kata 4

---
+++ White Belt/Kata 4/Kata 4/Program.cs
@@ -0,0 +1,9 @@
+﻿
+for (int i = 0; i < 2; i++)
+{Console.WriteLine($"Spawning wave {i+1}!");
+    for (int j = 0; j < 3; j++)
+    {
+        Console.WriteLine($"Enemmy #{j + 1} just spawned!");
+    }
+    Console.WriteLine($"Wave {i+1} complete!");
+}
