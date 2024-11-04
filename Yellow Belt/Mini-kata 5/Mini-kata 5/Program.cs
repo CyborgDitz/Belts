77380a1b1931be6a2d1f61f240fda84b819a5049
CyborgDitz <sebban.lol@gmail.com>
2024-11-04 09:07:27 +0100

    Mini Kata 5

---
+++ Yellow Belt/Mini-kata 5/Mini-kata 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Attack();
+Heal();
+void Attack()
+{
+    int damage = 5;
+    Console.WriteLine($"Player dealt {damage}. That's a lot of damage!");
+}
+void Heal()
+{
+    int healAmount = 1;
+    Console.WriteLine($"Player healed for {healAmount}. That's not a lot...");
+}
