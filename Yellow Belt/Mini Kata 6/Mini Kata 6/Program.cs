ac8a395713cba62f059e347b944f5a1bcb6dbead
CyborgDitz <sebban.lol@gmail.com>
2024-11-04 10:22:18 +0100

    Mini kata 6

---
+++ Yellow Belt/Mini Kata 6/Mini Kata 6/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] enemies ={"Goblin", "Orc", "Troll", "Money Wizard"};
+string[] inventory = {"Sword", "Shield", "Potion"};
+
+Console.WriteLine("A choice of enemy in the arena...:");
+for (int i = 0; i < enemies.Length; i++)
+{
+    Console.WriteLine(enemies[i]);
+}
+Console.WriteLine("Player inventory:");
+for (int i = 0; i < inventory.Length; i++)
+{
+    Console.WriteLine(inventory[i]);
+}
+string newItem = "Stick";
+string[] newInventory = new string[inventory.Length + 1];
+for (int i = 0; i < inventory.Length; i++)
+{
+    newInventory[i] = inventory[i];
+}
+newInventory[newInventory.Length-1] = newItem;
+
+Console.WriteLine("New Inventory:");
+for (int i = 0; i < newInventory.Length; i++)
+{
+ 
+    Console.WriteLine(newInventory[i]);
+}
