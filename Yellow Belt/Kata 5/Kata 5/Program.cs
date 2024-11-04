bf63456ed99fbb86d21eff56d9a50746720b8b13
CyborgDitz <sebban.lol@gmail.com>
2024-11-04 09:24:02 +0100

    Kata 5

---
+++ Yellow Belt/Kata 5/Kata 5/Program.cs
@@ -0,0 +1,23 @@
+﻿string enemyName = "Goblin King";
+string playerName = "Luke";
+int damage;
+
+void AttackEnemy() {
+    damage = 63;
+    Console.WriteLine($"{playerName} attacked {enemyName} and dealt {damage} damage!");
+}
+
+void EnemyAttack()
+{
+    Console.WriteLine($"{enemyName} retaliated and attacked {playerName}, dealing {damage} damage!");
+    damage = 45;
+}
+
+void HealPlayer() {
+    int healAmount = 50;
+    Console.WriteLine($"{playerName} cast heal, and healed themself for {healAmount} health!");
+}
+
+    AttackEnemy();
+    EnemyAttack();
+    HealPlayer();
