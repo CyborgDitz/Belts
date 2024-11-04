eb3679e1f18af8a63708376bddc754e31dfccd99
CyborgDitz <sebban.lol@gmail.com>
2024-11-04 10:52:43 +0100

    Used List instead haha

--- Yellow Belt/Mini Kata 6/Mini Kata 6/Program.cs
+++ Yellow Belt/Mini Kata 6/Mini Kata 6/Program.cs
@@ -1,26 +1,40 @@
 ﻿string[] enemies ={"Goblin", "Orc", "Troll", "Money Wizard"};
-string[] inventory = {"Sword", "Shield", "Potion"};
-
 Console.WriteLine("A choice of enemy in the arena...:");
 for (int i = 0; i < enemies.Length; i++)
 {
     Console.WriteLine(enemies[i]);
 }
-Console.WriteLine("Player inventory:");
-for (int i = 0; i < inventory.Length; i++)
-{
-    Console.WriteLine(inventory[i]);
-}
-string newItem = "Stick";
-string[] newInventory = new string[inventory.Length + 1];
-for (int i = 0; i < inventory.Length; i++)
-{
-    newInventory[i] = inventory[i];
-}
-newInventory[newInventory.Length-1] = newItem;
 
-Console.WriteLine("New Inventory:");
-for (int i = 0; i < newInventory.Length; i++)
+List<string> inventory = new List<string>();
+inventory.Add("Sword");
+inventory.Add("Shield");
+inventory.Add("Potion");
+Console.WriteLine("Player inventory:");
+foreach (string item in inventory)
 {
-    Console.WriteLine(newInventory[i]);
+    Console.WriteLine(item);
 }
+inventory.Add("Stick");
+Console.WriteLine("New Inventory:");
+foreach (string item in inventory)
+{
+    Console.WriteLine(item);
+}
+// Console.WriteLine(inventory);
+// for (int i = 0; i < inventory.Length; i++)
+// {
+//     Console.WriteLine(inventory[i]);
+// }
+// string newItem = "Stick";
+// string[] newInventory = new string[inventory.Length + 1];
+// for (int i = 0; i < inventory.Length; i++)
+// {
+//     newInventory[i] = inventory[i];
+// }
+// newInventory[newInventory.Length-1] = newItem;
+//
+// Console.WriteLine("New Inventory:");
+// for (int i = 0; i < newInventory.Length; i++)
+// {
+//     Console.WriteLine(newInventory[i]);
+// }
