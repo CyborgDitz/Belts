069e6fa3bd5692f14892d514bdb73777a544c7ba
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-11-08 16:14:50 +0100

    Remade kata 5

---
+++ White Belt/Kata 5/Kata 5/Program.cs
@@ -0,0 +1,17 @@
+﻿CombatRound();
+
+void CombatRound()
+{
+    AttackEnemy("Orc", 42);
+    HealPlayer("Oddball", 55);
+}
+
+void AttackEnemy(string enemyName, int damage)
+{
+    Console.Write($"Player attacks {enemyName} for {damage} damage!");
+}
+
+void HealPlayer(string playerName, int healAmount)
+{
+    Console.Write($"Player heals {playerName} for {healAmount} health!");
+}
