49bed1fbc171cabedb6e143962aceebda607d2fc
CyborgDitz <sebban.lol@gmail.com>
2024-11-14 12:01:33 +0100

    learning syntax in kata orange
    also rider is broken

--- Orange Belt/Kata 1 Orange/Character.cs
+++ Orange Belt/Kata 1 Orange/Character.cs
@@ -2,5 +2,11 @@ namespace ConsoleApp1;
 
 public class Character()
 {
-    
+    lambda syntax
+(variable)=> {
+string output = "Balababa";
+output += target.TakeDamage(10);
+return output;
+}
+and then shit happens
 }
