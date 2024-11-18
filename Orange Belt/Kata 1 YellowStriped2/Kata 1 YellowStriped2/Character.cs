088f7042051e3e703720f41dc73fbe3906429478
CyborgDitz <sebban.lol@gmail.com>
2024-11-18 11:00:31 +0100

    whatver

---
+++ Orange Belt/Kata 1 YellowStriped2/Kata 1 YellowStriped2/Character.cs
@@ -0,0 +1,6 @@
+public class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public Action PrimaryAction { get; set; }
+}
---
+++ Orange Belt/Kata 1 YellowStriped2/Kata 1 YellowStriped2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<Character> characters = new List<Character> 
+{
+    new(){
+        Name = "Warrior",
+        Health = 40, 
+        PrimaryAction = () => Console.WriteLine("Warrior attacks with sword!")
+    },
+    new()
+    {
+        Name = "Healer",
+        Health = 60, 
+        PrimaryAction = () => Console.WriteLine("Healer casts healing spell!")
+    },
+};
+
+       
+Action<List<Character>> lowHealthAttackers = chars =>
+{
+    foreach (var character in chars)
+    {
+        if (character.Health < 50 && character.Name != "Healer")
+        {
+            Console.WriteLine($"{character.Name} has low health (Health: {character.Health}), performing action...");
+            character.PrimaryAction.Invoke();
+            healer.P
+        }
+        }
+    }
+};
+
+        
+lowHealthAttackers(characters);
+
