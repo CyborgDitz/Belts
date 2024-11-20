fb8c0b900763f6306b51ec1432941494309ad855
CyborgDitz <sebban.lol@gmail.com>
2024-11-20 09:13:14 +0100

    Fix bug after Exam grade feedback

--- White Belt/Exam Kata WhiteBelt/Exam Kata WhiteBelt/Program.cs
+++ White Belt/Exam Kata WhiteBelt/Exam Kata WhiteBelt/Program.cs
@@ -4,12 +4,12 @@ int healthPlayer = 155;
 
 
 string nameEnemy = "Goblin King";
-int healthEnemy = 666;
+int healthEnemy = 300;
 
 
 Console.WriteLine("You face the mighty Goblin King in battle. What will you do?");
 // game while loop
-while (healthPlayer > 0 || healthEnemy > 0) {  
+while (healthPlayer > 0 && healthEnemy > 0) {  
     int atkPwrPlayer = random.Next(100,155);
     int atkPwrEnemy = random.Next(15,34);
     int healPlayer = random.Next(1,99);
@@ -19,20 +19,14 @@ while (healthPlayer > 0 || healthEnemy > 0) {
         switch (input2Low)
         {
             case "attack": {
-                //attack turn
                 Console.WriteLine($"{namePlayer} is attacking the {nameEnemy}");
                 Console.WriteLine($"{nameEnemy} takes {atkPwrPlayer} damage");
                 healthEnemy -= atkPwrPlayer;
-                // Console.WriteLine();
-                // Console.WriteLine($"{nameEnemy} attacks the {namePlayer}!");
-                // Console.WriteLine($"{namePlayer} takes {atkPwrEnemy}");
-                // healthPlayer -= atkPwrEnemy;
-                // Console.WriteLine($"The Hero has {healthPlayer} health left");
                 break;
             }
 
             case "heal":
-                { //heal turn
+                { 
                     if (healthPlayer == 155)
                     {
                         Console.WriteLine($"You can't heal yourself more!");
@@ -54,6 +48,10 @@ while (healthPlayer > 0 || healthEnemy > 0) {
               
                 
             }
+        if (healthEnemy <= 0) {
+            Console.WriteLine("The Goblin King is dead. You have saved the land!");
+            break;
+        }
         //enemy turn
         Console.WriteLine();
         Console.WriteLine($"{nameEnemy} attacks the {namePlayer}!");
@@ -61,11 +59,10 @@ while (healthPlayer > 0 || healthEnemy > 0) {
         healthPlayer -= atkPwrEnemy;
         Console.WriteLine($"The Hero has {healthPlayer} health left");
 
-        if (healthEnemy <= 0) {
-            Console.WriteLine("The Goblin King is dead. You have saved the land!");
-        }
-        else if (healthPlayer <= 0){
+        
+        if (healthPlayer <= 0){
             Console.WriteLine("You died");
+            break;
         }
-        //round over
+        
 }
