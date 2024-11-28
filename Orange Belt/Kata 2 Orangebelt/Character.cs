0ed1c8a3e1a28c2eece96b3c82414bd801682c7e
CyborgDitz <sebban.lol@gmail.com>
2024-11-28 12:16:00 +0100

    Kata 2 Orange belt, new folder and Feedback fix

---
+++ Orange Belt/Kata 2 Orangebelt/Character.cs
@@ -0,0 +1,25 @@
+namespace ConsoleApp1;
+
+public class Character : IAttack 
+{ 
+    private string _name { get; set; } //Encapsulation 
+    private int _health { get; set; } //Encapsulation
+    private int _damage { get; set; }
+
+    public Character(string name, int health, int damage)//Encapsulation
+    {
+        _name = name;
+        _health = health;
+        _damage = damage;
+    }
+    public event Action<int> HealthChanged; //Meaningful names
+  //Meaningful names
+    
+    public void Attack(Character target) //Meaningful names
+    {
+        target._health -= _damage;
+        Console.WriteLine($"{_name} attacked {target._name} for {_damage} damage!");
+        target.HealthChanged?.Invoke(target._health); //Separation of Concerns
+    }
+
+}
---
+++ Orange Belt/Kata 2 Orangebelt/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1;
+
+Character goblino = new Character("Goblino", 55, 50); //OCP 
+Character player = new Character("Player", 111, 5);
+goblino.HealthChanged += OnHealthChanged;
+player.HealthChanged += OnHealthChanged; //DRy fix
+
+player.Attack(goblino); //KISS (Keep it simple, stupid) //Separation of Concerns
+goblino.Attack(player);
+
+static void OnHealthChanged(int newHealth) //OCP
+{
+    Console.WriteLine($"[Event] Character's health changed to {newHealth}.");
+}
