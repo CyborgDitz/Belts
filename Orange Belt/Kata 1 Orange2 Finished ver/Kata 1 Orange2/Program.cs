15d4e12b4129f4d7b986b11ddfda19182e33e812
CyborgDitz <sebban.lol@gmail.com>
2024-11-28 13:51:06 +0100

    Publish i guess.

--- Orange Belt/Kata 1 Orange2 Finished ver/Kata 1 Orange2/Program.cs
+++ Orange Belt/Kata 1 Orange2 Finished ver/Kata 1 Orange2/Program.cs
@@ -1,24 +1,19 @@
 ﻿using Kata_1_Orange2;
 Character warrior = new Character ("Warrior",45 );
-Character healer = new Character("Healer", 51);
-healer.PrimaryAction = (target) => Console.WriteLine($"{healer.Name} performs a powerful healing spell!");
-warrior.PrimaryAction = (target) => Console.WriteLine($"{warrior.Name} is attacking!");
+Character healer = new Character("Healer", 65);
 List<Character> characters = new List<Character> { warrior, healer };
+healer.PrimaryAction = (character) => Console.WriteLine($"{healer.Name} performs a powerful healing spell!");
+warrior.PrimaryAction = (character) => 
+    Console.WriteLine("Warrior is attacking for big damuge!");
 
-
-Action<Character> attackFirstAction = character =>
-{var charFirst = characters.First();
-    Console.WriteLine($"{character.Name} is acting first due to low health! ({character.Health} < 50");
-    character.PrimaryAction([charFirst]);
-};
-
-Action<List<Character>> lowHealthAttackers = (characters) =>
+Action<List<Character>> lowHealthAttackers = (lowHpChar) =>
 {
-    foreach (Character character in characters)
+    foreach (Character character in lowHpChar)
     {
         if (character.Health < 50)
         {
-            attackFirstAction(character);
+            Console.WriteLine($"{character.Name} is acting first due to low health! ({character.Health} < 50");
+            character.PrimaryAction(lowHpChar);
             break;
         }
     }
